a0a72844f2f627456e7dc8c133caf86e8392b8b4
han254 <peternjenga1706@gmail.com>
2022-04-24 07:33:14 +0300

    add a return value

--- src/main/java/org/example/encoding.java
+++ src/main/java/org/example/encoding.java
@@ -15,13 +15,17 @@ public class encoding {
     }
 
     public String cText() {
+        String cipherText="";
         try {
+            System.out.print("Enter plaintext>>");
             InputStreamReader streamReader = new InputStreamReader(System.in);
             BufferedReader bufferedReader = new BufferedReader(streamReader);
             String plaintext = bufferedReader.readLine();
+            System.out.print("Your Text is >>" +plaintext);
         } catch (Exception e) {
-
+        e.printStackTrace();
         }
+        return cipherText;
     }
 }
 
