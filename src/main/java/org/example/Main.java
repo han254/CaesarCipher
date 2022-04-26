e232750a0ec5f0c721acc50ff46ef226da151bae
han254 <peternjenga1706@gmail.com>
2022-04-26 11:38:54 +0300

    ignored files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+public class Main {
+    public static encoding Encoding =new encoding();
+    public static void main(String[] args) {
+        Encoding.userInput();
+        System.out.println("This is Your Ciphertext>>"+ Encoding.cText());
+    }
+
+    }
